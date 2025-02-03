fe6035548af54b217b8e6ec1c6d8a56cd10a5986
AidenLortie <146017618+AidenLortie@users.noreply.github.com>
2025-02-03 09:23:52 -0500

    Updated some stuff

--- js/header.js
+++ js/header.js
@@ -1,9 +1,6 @@
 navHide = document.getElementById("navHide");
 nav = document.getElementById("headerNav");
 
-console.log(navHide);
-console.log(nav);
-
 navHide.addEventListener("click", (e) => {
     nav.classList.toggle("hidden");
     navHide.src = nav.classList.contains("hidden") ? "./img/down.svg" : "./img/up.svg";
